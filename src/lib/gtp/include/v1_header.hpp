07d6b0d06231ee646e458c0d69714423cc197ce7
herlesupreeth <herlesupreeth@gmail.com>
2021-07-07 16:18:14 +0200

    add: Initial commit

---
+++ src/lib/gtp/include/v1_header.hpp
@@ -0,0 +1,64 @@
+//
+// This file is a part of n3gpp_access open source project.
+// Copyright (c) 2021 Supreeth Herle.
+//
+// The software and all associated files are licensed under GPL-3.0
+// and subject to the terms and conditions defined in LICENSE file.
+//
+
+#pragma once
+
+#include <string>
+#include <functional>
+#include <iostream>
+#include <optional>
+#include <cstdint>
+#include <boost/endian/arithmetic.hpp>
+
+namespace gtp {
+
+enum class GtpMessageType {
+  echoRequest = 1,
+  echoResponse = 2,
+  createPdpContextReq = 16,
+  createPdpContextRes = 17,
+  updatePdpContextReq = 18,
+  updatePdpContextRes = 19,
+  deletePdpContextReq = 20,
+  deletePdpContextRes = 21,
+  errorIndication = 26,
+  pduNotificationReq = 27,
+  supportedExtensionHeadersNotification = 31,
+  endMarker = 254,
+  gPdu = 255,
+};
+
+class GtpV1Header {
+ public:
+  GtpV1Header(GtpMessageType message_type, uint32_t teid, uint16_t seq_num = 0,
+              uint8_t n_pdu_num = 0, uint8_t nxt_ext_hdr_type = 0);
+  ~GtpV1Header();
+
+  uint16_t GetLength();
+  uint16_t SetLength(uint16_t length);
+  uint32_t GetTeid();
+  uint32_t SetTeid(uint32_t teid);
+  uint16_t GetSequenceNumber();
+  uint16_t SetSequenceNumber(uint16_t seq_num);
+
+ private:
+  uint8_t version_ : 3;
+  uint8_t pt_ : 1;
+  uint8_t reserved_ : 1;
+  uint8_t e_ : 1;
+  uint8_t s_ : 1;
+  uint8_t pn_ : 1;
+  uint8_t message_type_;
+  boost::endian::big_uint16_t length_;
+  boost::endian::big_uint32_t teid_;
+  std::optional<boost::endian::big_uint16_t> seq_num_ = std::nullopt;
+  std::optional<uint8_t> n_pdu_num_ = std::nullopt;
+  std::optional<uint8_t> nxt_ext_hdr_type_ = std::nullopt;
+};
+
+} // namespace gtp
---
+++ src/lib/gtp/src/v1_header.cpp
@@ -0,0 +1,65 @@
+//
+// This file is a part of n3gpp_access open source project.
+// Copyright (c) 2021 Supreeth Herle.
+//
+// The software and all associated files are licensed under GPL-3.0
+// and subject to the terms and conditions defined in LICENSE file.
+//
+
+#include <cstdint>
+
+#include "../include/v1_header.hpp"
+
+namespace gtp {
+
+GtpV1Header::GtpV1Header(GtpMessageType message_type, uint32_t teid, uint16_t seq_num,
+							uint8_t n_pdu_num, uint8_t nxt_ext_hdr_type)
+	: teid_(teid) {
+
+	version_ = 0x1;
+	pt_ = 0x1;
+	reserved_ = 0x0;
+	message_type_ = static_cast<uint8_t>(message_type);
+
+	if (seq_num != 0) s_ = 0x1;
+	if (n_pdu_num != 0) pn_ = 0x1;
+	if (nxt_ext_hdr_type != 0) e_ = 0x1;
+
+	bool flag = s_ | pn_ | e_;
+	if (flag) {
+		seq_num_ = seq_num;
+		n_pdu_num_ = n_pdu_num;
+		nxt_ext_hdr_type_ = nxt_ext_hdr_type;
+	}
+};
+
+GtpV1Header::~GtpV1Header() {}
+
+uint16_t GtpV1Header::GetLength() {
+ return length_;
+}
+
+uint16_t GtpV1Header::SetLength(uint16_t length) {
+	length_ = length;
+}
+
+uint32_t GtpV1Header::GetTeid() {
+	return teid_;
+}
+
+uint32_t GtpV1Header::SetTeid(uint32_t teid) {
+	teid_ = teid;
+}
+
+uint16_t GtpV1Header::GetSequenceNumber() {
+	if (seq_num_.has_value()) {
+		return seq_num_.value();
+	}
+	return 0;
+}
+
+uint16_t GtpV1Header::SetSequenceNumber(uint16_t seq_num) {
+	seq_num_ = seq_num;
+}
+
+} // namespace gtp
