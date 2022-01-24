aaf6950f4ca062a97095e4ecbc465090de5e6c37
herlesupreeth <herlesupreeth@gmail.com>
2022-01-24 15:05:40 +0100

    Intro to writing unit test cases

---
+++ libs/common/tests/logger_test.cc
@@ -0,0 +1,13 @@
+//
+// This file is a part of n3gpp_access open source project.
+// Copyright (c) 2021 Supreeth Herle.
+//
+// The software and all associated files are licensed under GPL-3.0
+// and subject to the terms and conditions defined in LICENSE file.
+//
+
+#include <gtest/gtest.h>
+
+TEST(LoggerAssertTest, LoggerTestSuite) {
+  ASSERT_TRUE(true);
+}
